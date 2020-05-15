704b883310a48d30a93e7f147bec9738599389bb
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-15 21:06:31 +0200

    Initial Commit

---
+++ IZA-proj2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IZA-proj2
+//
+//  Created by Lukas Javorsky on 15/05/2020.
+//  Copyright © 2020 Lukas Javorsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
