bb61ae8343c67c4b84de5f9f2da07f3edd1dbe7c
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-18 13:43:54 +0200

    Implement the StudentData class

--- StudentRating/StudentRating/StudentData.swift
+++ StudentRating/StudentRating/StudentData.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Lukas Javorsky. All rights reserved.
 //
 
-import Foundation
+import CoreData
+
+public class StudentData:NSManagedObject, Identifiable {
+    // Atributes in StudentData entity
+    @NSManaged public var login:String?
+    @NSManaged public var rating:String?
+}
+
+extension StudentData {
+    // MARK: - Get the data from database
+    static func getAllData() -> NSFetchRequest<StudentData>{
+        let myRequest:NSFetchRequest<StudentData> = StudentData.fetchRequest() as! NSFetchRequest<StudentData>
+        
+        let sortDescriptor = NSSortDescriptor(key: "login", ascending: true)
+        
+        myRequest.sortDescriptors = [sortDescriptor]
+        
+        return myRequest
+    }
+}
