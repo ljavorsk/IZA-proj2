3b54a76de2853e482681d52d9e8c80f6f59d78ef
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-18 13:45:47 +0200

    Create extensions folder
    Add extension for the app color

---
+++ StudentRating/StudentRating/Extensions/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  StudentRating
+//
+//  Created by Lukas Javorsky on 17/05/2020.
+//  Copyright © 2020 Lukas Javorsky. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static var appColor = Color(UIColor.systemTeal)
+}
