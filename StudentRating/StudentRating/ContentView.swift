18ee06b648919991b2f919d6f147faaf6a630031
Lukas Javorsky <ljavorsk@redhat.com>
2020-05-18 13:29:06 +0200

    Add content view
    Content view decides whather to show onboarding view or the actual app

--- StudentRating/StudentRating/ContentView.swift
+++ StudentRating/StudentRating/ContentView.swift
@@ -8,9 +8,25 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    /// State that checks if the onboarding screen is active
+    @State private var hasSeenOnboarding = UserDefaults.standard.bool(forKey: "hasSeenOnboarding")
+    /// Settings that stores if the user had already seen the onboarding
+    @ObservedObject var settings = UserSettings()
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        Group {
+            if !hasSeenOnboarding && !settings.continuePressed {
+                // MARK: - Introduction view for onboarding informations
+                IntroView(continuePressed: self.$settings.continuePressed)
+            } else {
+                // MARK: - Main screen with the actual application
+                MainContentView()
+            }
+        }
     }
 }
 
